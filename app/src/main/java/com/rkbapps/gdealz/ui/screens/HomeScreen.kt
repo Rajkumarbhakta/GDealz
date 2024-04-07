cfb55b7bad6074d6720cf98956b6fbb578f8e762
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2024-04-07 23:57:28 +0530

    Initial .

--- app/src/main/java/com/rkbapps/gdealz/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/rkbapps/gdealz/ui/screens/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.rkbapps.gdealz.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -11,6 +16,11 @@ class HomeScreen:Screen {
         val context = LocalContext.current
         val navigator = LocalNavigator.current
 
+        Scaffold {
+            Column(modifier = Modifier.fillMaxSize().padding(it)) {
+
+            }
+        }
 
     }
 }
