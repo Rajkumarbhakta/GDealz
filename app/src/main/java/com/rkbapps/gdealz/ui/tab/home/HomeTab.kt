5df5a17a4801631d097dab3867513870a6eec33e
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2024-04-08 13:39:20 +0100

    deals page home page design

--- app/src/main/java/com/rkbapps/gdealz/ui/tab/home/HomeTab.kt
+++ app/src/main/java/com/rkbapps/gdealz/ui/tab/home/HomeTab.kt
@@ -1,5 +1,6 @@
 package com.rkbapps.gdealz.ui.tab.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
@@ -23,6 +23,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,7 +40,7 @@ fun HomeTab(
 ) {
     val viewModel: HomeTabViewModel = hiltViewModel()
     val deals = viewModel.deals.collectAsState()
-    Scaffold {innerPadding->
+    Scaffold { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -74,19 +75,36 @@ fun DealsItem(deals: Deals) {
     Card(
         elevation = CardDefaults.cardElevation(10.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier
+            .height(150.dp)
+            .padding(8.dp)
     ) {
-        Column {
-            AsyncImage(model = deals.thumb, contentDescription = "game thumb")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = deals.thumb,
+                contentDescription = "game thumb", modifier = Modifier.height(50.dp)
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = deals.title ?: "")
-            Row {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(8.dp)
+            ) {
                 Text(
-                    text = "${deals.normalPrice}",
-                    style = TextStyle(textDecoration = TextDecoration.LineThrough)
+                    text = deals.title ?: "",
+                    maxLines = 2,
+                    style = TextStyle(fontWeight = FontWeight.Bold)
                 )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "${deals.salePrice}")
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "$${deals.normalPrice}",
+                        style = TextStyle(textDecoration = TextDecoration.LineThrough)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = "$${deals.salePrice}")
+                }
             }
 
         }
