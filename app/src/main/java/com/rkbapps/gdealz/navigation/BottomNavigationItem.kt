7589494b0bf4e9ba8da39e62ea7df1794d57c0cf
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2026-05-09 17:58:26 +0530

    Refactor BottomNavigationItem to use string resources for improved localization

--- app/src/main/java/com/rkbapps/gdealz/navigation/BottomNavigationItem.kt
+++ app/src/main/java/com/rkbapps/gdealz/navigation/BottomNavigationItem.kt
@@ -1,22 +1,47 @@
 package com.rkbapps.gdealz.navigation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.rkbapps.gdealz.R
 
 sealed class BottomNavigationItem(
     val route: Routes,
-    val title: String,
-    val icon: Int,
-    val selectedIcon:Int
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int,
+    @DrawableRes val selectedIcon: Int
 ) {
-    data object Deals : BottomNavigationItem(Routes.Deals, "Deals",
-        R.drawable.cart_outlined,R.drawable.cart_filled)
+    data object Deals : BottomNavigationItem(
+        Routes.Deals, R.string.tab_deals,
+        R.drawable.cart_outlined, R.drawable.cart_filled
+    )
+
     data object Search :
-        BottomNavigationItem(Routes.Search, title = "Search", icon = R.drawable.search, selectedIcon = R.drawable.search)
+        BottomNavigationItem(
+            Routes.Search,
+            title = R.string.search,
+            icon = R.drawable.search,
+            selectedIcon = R.drawable.search
+        )
 
     data object Free :
-        BottomNavigationItem(Routes.FreeDeals, title = "Free", icon = R.drawable.gift_outlined, selectedIcon = R.drawable.gift_filled)
+        BottomNavigationItem(
+            Routes.FreeDeals,
+            title = R.string.free_title,
+            icon = R.drawable.gift_outlined,
+            selectedIcon = R.drawable.gift_filled
+        )
 
-    data object Fav : BottomNavigationItem(Routes.Fav, title = "Fav", icon = R.drawable.fav_outlined,R.drawable.fav_filled)
+    data object Fav : BottomNavigationItem(
+        Routes.Fav,
+        title = R.string.tab_fav,
+        icon = R.drawable.fav_outlined,
+        R.drawable.fav_filled
+    )
 
-    data object Settings : BottomNavigationItem(Routes.Settings, title = "Settings", icon = R.drawable.settings_outline, selectedIcon = R.drawable.settings_filled)
+    data object Settings : BottomNavigationItem(
+        Routes.Settings,
+        title = R.string.settings,
+        icon = R.drawable.settings_outline,
+        selectedIcon = R.drawable.settings_filled
+    )
 }
