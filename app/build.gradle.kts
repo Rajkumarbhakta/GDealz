7ef7a3b21fa2ed3f31e03e22c28ebc1bc636a1ed
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2026-06-26 22:10:33 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.rkbapps.gdealz"
         minSdk = 23
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.3.0"
+        versionCode = 11
+        versionName = "1.3.1"
 
         // build configs
         buildConfigField("String", "API_KEY", properties.getProperty("API_KEY"))
